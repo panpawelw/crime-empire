3b63061f0c5847ba2ee1e9e5c108a4072ac49228
pjm77 <probably.just.me77@gmail.com>
2018-01-10 12:47:35 +0100

    Initial commit

---
+++ src/main/java/pl/lococompadres/app/AppConfig.java
@@ -0,0 +1,63 @@
+package pl.lococompadres.app;
+
+import java.util.Locale;
+
+import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer{
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean(name="localeResolver")
+	public LocaleContextResolver getLocaleContextResolver() {
+	SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	localeResolver.setDefaultLocale(new Locale("pl","PL"));
+	return localeResolver; }
+	
+	@Bean
+	public Validator validator() {
+	return new LocalValidatorFactoryBean();
+	}
+
+	@Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory() {
+		LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+		emfb.setPersistenceUnitName("CrimeEmpirePersistenceUnit");
+		return emfb;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager tm = new JpaTransactionManager(emf);
+		return tm;
+	}
+
+}
